59b579d4ff04bce8fe9d09eb55d6f5f9f377c179
Andrew Healey <healeycodes@gmail.com>
2023-07-23 14:02:03 +0100

    Add subprocess

--- api/index.py
+++ api/index.py
@@ -1,8 +1,23 @@
+import subprocess
 from flask import Flask, request
 
 app = Flask(__name__)
 
 
 @app.route("/api/exec", methods=['POST'])
-def hello_world():
-    return str(request.json())
+def exec():
+    code = request.get_json()['code']
+    proc = subprocess.Popen(["ls -a"])
+    try:
+        out, err = proc.communicate(timeout=3)
+    except subprocess.TimeoutExpired:
+        proc.kill()
+        out, err = proc.communicate()
+    return f'''
+(stdout)
+{out}
+(stderr)
+{err}
+(code)
+{code}
+'''
